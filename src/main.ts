11d66cfb63b8706ed75e344c75b44e1088e136bd
bahareh <baharehviani@gmail.com>
2026-04-26 13:38:54 +0330

    .env and log port

--- src/main.ts
+++ src/main.ts
@@ -20,6 +20,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
   await seedUsers();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
+  
+  console.log("running on port", process.env.PORT)
 }
 bootstrap();
